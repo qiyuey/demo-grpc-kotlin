517b03ba51501a3899faa3a6b34f401a1910969e
qiyuey <qiyuey@vip.qq.com>
2020-04-19 16:11:59 +0800

    ADD: Time

--- demo-grpc-kotlin-client/src/main/kotlin/top/qiyuey/demo/HelloClient.kt
+++ demo-grpc-kotlin-client/src/main/kotlin/top/qiyuey/demo/HelloClient.kt
@@ -2,33 +2,37 @@ package top.qiyuey.demo
 
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.asExecutor
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 import top.qiyuey.demo.hello.GreeterGrpcKt
 import top.qiyuey.demo.hello.HelloRequest
 import java.io.Closeable
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.TimeUnit
 
-class HelloClient(
+class HelloClient private constructor(
         private val channel: ManagedChannel
 ) : Closeable {
+
     private val stub: GreeterGrpcKt.GreeterCoroutineStub = GreeterGrpcKt.GreeterCoroutineStub(channel)
+
+    constructor(
+            channelBuilder: ManagedChannelBuilder<*>,
+            dispatcher: CoroutineDispatcher
+    ) : this(channelBuilder.executor(dispatcher.asExecutor()).build())
 
     suspend fun greet(name: String) = coroutineScope {
         val request = HelloRequest.newBuilder().setName(name).build()!!
         val response = stub.sayHello(request)
-        println("Received: ${response.message}")
+        println("Received[${LocalTime.now().format(DateTimeFormatter.ISO_TIME)}]: ${response.message}")
     }
 
     suspend fun greets(names: Collection<String>) = coroutineScope {
-        val requests = names.asFlow().map { HelloRequest.newBuilder().setName(it).build()!! }
+        val requests = names.map { HelloRequest.newBuilder().setName(it).build()!! }.asFlow()
         stub.sayHellos(requests).collect {
-            println("Received: ${it.message}")
+            println("Received[${LocalTime.now().format(DateTimeFormatter.ISO_TIME)}]: ${it.message}")
         }
     }
 
@@ -37,22 +41,16 @@ class HelloClient(
     }
 }
 
-
-/**
- * Greeter, uses first argument as name to greet if present;
- * greets "world" otherwise.
- */
-fun main() = runBlocking {
+fun main(): Unit = runBlocking {
     val port = 50051
-
-    val client = HelloClient(
-            ManagedChannelBuilder.forAddress("localhost", port)
-                    .usePlaintext()
-                    .executor(Dispatchers.Default.asExecutor())
-                    .build())
-
-    client.greet("world")
-
-    val users = (0..6).map { it.toString() }
-    client.greets(users)
+    val users = (1..6).map { it.toString() }
+    val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext()
+    HelloClient(channel, Dispatchers.Default).use {
+        coroutineScope {
+            launch { it.greets(users) }
+            delay(4000)
+            launch { it.greet("world") }
+        }
+    }
+    return@runBlocking
 }
