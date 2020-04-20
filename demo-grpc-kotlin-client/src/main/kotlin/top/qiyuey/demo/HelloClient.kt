4287d455a037cf95258b22acf3c4549ecdde0585
于川 <1123274330@qq.com>
2020-04-20 15:21:06 +0800

    MOD: 参数 flow 化

--- demo-grpc-kotlin-client/src/main/kotlin/top/qiyuey/demo/HelloClient.kt
+++ demo-grpc-kotlin-client/src/main/kotlin/top/qiyuey/demo/HelloClient.kt
@@ -3,8 +3,10 @@ package top.qiyuey.demo
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import top.qiyuey.demo.hello.GreeterGrpcKt
 import top.qiyuey.demo.hello.HelloRequest
 import java.io.Closeable
@@ -29,8 +31,8 @@ class HelloClient private constructor(
         println("Received[${LocalTime.now().format(DateTimeFormatter.ISO_TIME)}]: ${response.message}")
     }
 
-    suspend fun greets(names: Collection<String>) = coroutineScope {
-        val requests = names.map { HelloRequest.newBuilder().setName(it).build()!! }.asFlow()
+    suspend fun greets(names: Flow<String>) = coroutineScope {
+        val requests = names.map { HelloRequest.newBuilder().setName(it).build()!! }
         stub.sayHellos(requests).collect {
             println("Received[${LocalTime.now().format(DateTimeFormatter.ISO_TIME)}]: ${it.message}")
         }
@@ -47,7 +49,7 @@ fun main(): Unit = runBlocking {
     val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext()
     HelloClient(channel, Dispatchers.Default).use {
         coroutineScope {
-            launch { it.greets(users) }
+            launch { it.greets(users.asFlow()) }
             delay(4000)
             launch { it.greet("world") }
         }
