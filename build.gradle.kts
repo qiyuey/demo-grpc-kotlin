df34069b7758201d988bbc6e6ae5c52ad0ff31a2
于川 <1123274330@qq.com>
2020-06-13 21:09:03 +0800

    MOD: 更新版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,17 +10,17 @@ plugins {
 
 subprojects {
     buildscript {
-        extra["protobufVersion"] = "3.11.4"
+        extra["protobufVersion"] = "3.12.2"
     }
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
         imports {
             mavenBom("org.jetbrains.kotlin:kotlin-bom:1.3.72")
-            mavenBom("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.3.5")
-            mavenBom("io.grpc:grpc-bom:1.28.1")
+            mavenBom("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.3.7")
+            mavenBom("io.grpc:grpc-bom:1.30.0")
         }
         dependencies {
-            dependency("io.grpc:grpc-kotlin-stub:0.1.1")
+            dependency("io.grpc:grpc-kotlin-stub:0.1.3")
             dependency("javax.annotation:javax.annotation-api:1.3.2")
         }
     }
